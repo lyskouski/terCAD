6b5261c265fa5776d1b85a769ad8faca54f6dc06
Viachaslau Lyskouski <lyskouski@github.com>
2022-11-22 12:48:15 +0000

    (#1) Update Flutter template to a basic view. Add Mocked Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,53 @@
 import 'package:flutter/material.dart';
+import './mainMenu.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  static const appTitle = 'terCAD';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'terCAD',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Data Quality Gate Portal for Educational Purposes'),
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        leading: Icon(Icons.menu),
+      appBar: AppBar(title: Text(title)),
+      body: const Center(
+        child: Text('By now, content is missing on this page. Please visit it later...'),
       ),
-      body: Text(
-        'Data Quality Gate Portal for Educational Purposes',
-        style: Theme.of(context).textTheme.headline4,
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const SizedBox(
+              height: 42.0,
+              child: DrawerHeader(
+                margin: EdgeInsets.all(0.0),
+                padding: EdgeInsets.all(paddingIndent),
+                child: Text('Navigation'),
+              ),
+            ),
+            ...mainMenu.map(buildMainMenu).toList()
+          ],
+        ),
       ),
     );
   }
---
+++ lib/mainMenu.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+const double paddingIndent = 12;
+
+class BasicTile {
+  final String title;
+  final List<BasicTile> tiles;
+
+  const BasicTile({
+    required this.title,
+    this.tiles = const [],
+  });
+}
+
+Widget buildMainMenu(BasicTile tile, {double leftIndent = paddingIndent}) {
+  if (tile.tiles.isEmpty) {
+    return ListTile(
+      contentPadding: EdgeInsets.only(left: leftIndent),
+      title: Text(tile.title),
+      onTap: () => {
+        // ...
+      },
+    );
+  } else {
+    return ExpansionTile(
+      tilePadding: EdgeInsets.only(left: leftIndent),
+    //  trailing: const SizedBox.shrink(),
+    //  leading: const Icon(Icons.keyboard_arrow_right_outlined),
+      title: Text(tile.title),
+      children: tile.tiles.map((tile) => buildMainMenu(tile, leftIndent: paddingIndent + leftIndent)).toList(),
+    );
+  }
+}
+
+final mainMenu = <BasicTile>[
+  const BasicTile(
+    title: 'Oeuvre',
+    tiles: [
+      BasicTile(title: 'Prose'),
+      BasicTile(title: 'Poetry'),
+    ],
+  ),
+  const BasicTile(
+    title: 'Cognition',
+  ),
+  const BasicTile(
+    title: 'Mind',
+    tiles: [
+      BasicTile(
+        title: 'Methods',
+        tiles: [
+          BasicTile(
+            title:'By sensory modality',
+          ),
+          BasicTile(
+            title: 'In Content',
+          ),
+          BasicTile(
+            title: 'Memory Organization',
+          ),
+          BasicTile(
+            title: 'Time Characteristics',
+          ),
+          BasicTile(
+            title: 'Presence of Target',
+          ),
+          BasicTile(
+            title: 'Availability of Means',
+          ),
+          BasicTile(
+            title: 'Method for Storing',
+          ),
+          BasicTile(
+            title: 'Symbolic Memory',
+          ),
+        ],
+      ),
+      BasicTile(
+        title: 'Objects',
+        tiles: [
+          BasicTile(
+            title:'Exact Sciences',
+          ),
+          BasicTile(
+            title:'Social Sciencies',
+          ),
+          BasicTile(
+            title:'Humanities',
+          ),
+          BasicTile(
+            title:'Music Education',
+          ),
+        ]
+      ),
+    ],
+  ),
+];
